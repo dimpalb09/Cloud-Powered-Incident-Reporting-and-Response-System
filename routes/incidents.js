847d72af0e2411f01b79404c691f10b793b52ea0
Dimpal Bhangale <126650152+dimpalb09@users.noreply.github.com>
2025-09-18 20:24:13 +0530

    Delete routes directory

--- routes/incidents.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const incidentController = require('../controllers/incidentController');
-
-router.post('/report', incidentController.reportIncident);
-router.get('/all', incidentController.getAllIncidents);
-
-module.exports = router;
